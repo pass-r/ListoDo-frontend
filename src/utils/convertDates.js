43d4a9d780360c8e85be25aa78558c684e80f7e3
Pascal Rivoire <117156701+justausercoding@users.noreply.github.com>
2025-06-21 12:49:29 +0200

    Rename file and function, update JSDoc.

---
+++ src/utils/convertDates.js
@@ -0,0 +1,21 @@
+/**
+ * Converts data string fields within each task object in the array to JavaScript Date objects.
+ *
+ * This function processes each task and converts the 'addedDate', 'dueDate' and 'doneDate'
+ * fields from strings to 'Date' objects, if present. If any of those fields are missing or empty,
+ * they will be set to 'null'.
+ *
+ * @param {Object[]} tasksArray - The array of task objects
+ * @returns {Object[]} A new array of task objects with date strings converted to 'Date' obejcts
+ * of 'null' if no valid date was provided.
+ */
+function convertDates(taskArray) {
+  return taskArray.map((taskObject) => ({
+    ...taskObject,
+    addedDate: taskObject.addedDate ? new Date(taskObject.addedDate) : null,
+    dueDate: taskObject.dueDate ? new Date(taskObject.dueDate) : null,
+    doneDate: taskObject.doneDate ? new Date(taskObject.doneDate) : null,
+  }));
+}
+
+export { convertDates };
