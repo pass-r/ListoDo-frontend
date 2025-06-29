aae0c44f48e0c4156bbd83c66ae64fe98148d5f6
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-06-29 17:40:00 +0200

    Add closeSidebar() after changing a view

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -6,34 +6,57 @@ import styles from "../../style/Sidebar.module.css";
 const createClassName = (currentView, viewName) =>
   `${styles.link} ${currentView === viewName ? styles.active : ""}`;
 
-export default function Sidebar({ currentView, setCurrentView, openModal }) {
+export default function Sidebar({
+  currentView,
+  setCurrentView,
+  openModal,
+  closeSidebar,
+}) {
   const { getProjects } = useContext(TasksContext);
   return (
     <>
-      <button className={`${styles.link} ${styles.newTask}`} onClick={openModal}>
+      <button
+        className={`${styles.link} ${styles.newTask}`}
+        onClick={() => {
+          openModal();
+          closeSidebar();
+        }}
+      >
         + ADD TASK
       </button>
       <button
         className={createClassName(currentView.active, "today")}
-        onClick={() => setCurrentView({ active: "today", isProject: false })}
+        onClick={() => {
+          setCurrentView({ active: "today", isProject: false });
+          closeSidebar();
+        }}
       >
         Today
       </button>
       <button
         className={createClassName(currentView.active, "future")}
-        onClick={() => setCurrentView({ active: "future", isProject: false })}
+        onClick={() => {
+          setCurrentView({ active: "future", isProject: false });
+          closeSidebar();
+        }}
       >
         Future
       </button>
       <button
         className={createClassName(currentView.active, "someday")}
-        onClick={() => setCurrentView({ active: "someday", isProject: false })}
+        onClick={() => {
+          setCurrentView({ active: "someday", isProject: false });
+          closeSidebar();
+        }}
       >
         Someday
       </button>
       <button
         className={createClassName(currentView.active, "completed")}
-        onClick={() => setCurrentView({ active: "completed", isProject: false })}
+        onClick={() => {
+          setCurrentView({ active: "completed", isProject: false });
+          closeSidebar();
+        }}
       >
         Completed
       </button>
@@ -44,7 +67,10 @@ export default function Sidebar({ currentView, setCurrentView, openModal }) {
         <button
           key={projectName}
           className={createClassName(currentView.active, projectName)}
-          onClick={() => setCurrentView({ active: projectName, isProject: true })}
+          onClick={() => {
+            setCurrentView({ active: projectName, isProject: true });
+            closeSidebar();
+          }}
         >
           {projectName}
         </button>
