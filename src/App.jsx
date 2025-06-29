08f386de5ee677eb8c192ecf6d060e9d2aa9a83a
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-06-29 17:34:20 +0200

    Add responsiveness to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,20 @@ import {
 } from "./components/tasks/taskviews/index.js";
 import TaskModal from "./components/tasks/TaskModal.jsx";
 import ConfirmDialog from "./components/notifications/ConfirmDialog.jsx";
+import imgMenu from "./assets/menu.svg";
+import imgClose from "./assets/close.svg";
 import styles from "./style/App.module.css";
 import "./style/global.css";
 
 export default function App() {
   const [currentView, setCurrentView] = useState({ active: "today", isProject: false });
   const [modalState, setModalState] = useState({ isOpen: false, changeId: null });
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const { isConfirmOpen, openConfirm, confirmDelete, cancelDelete } = useConfirmDelete();
 
   const openEditModal = (id) => setModalState({ isOpen: true, changeId: id });
+  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
 
   const currentViewComponent = () => {
     if (!currentView.isProject) {
@@ -42,23 +46,42 @@ export default function App() {
       return projectView;
     }
   };
-
-  // TODO: Loading????
-  // if (!data) {
-  //   return <h1>Loading</h1>;
-  // }
-
+  console.log("isSidebarOpen: ", isSidebarOpen);
   return (
     <TaskProvider>
       <div className={styles.gridLayout}>
-        <nav className={styles.sidebarContainer}>
+        <div className={styles.logoContainer}>
+          <div>
+            <p>ListoDo</p>
+            <div className={styles.logoSquare}></div>
+          </div>
+          <button className={styles.buttonShowSidebar} onClick={toggleSidebar}>
+            <img
+              src={isSidebarOpen ? imgClose : imgMenu}
+              alt={
+                isSidebarOpen
+                  ? "Button to open the sidebar"
+                  : "Button to open the sidebar"
+              }
+            />
+          </button>
+        </div>
+
+        <nav
+          className={`${styles.sidebarContainer} ${isSidebarOpen ? styles.sidebarOpen : ""}`}
+        >
           <Sidebar
             currentView={currentView}
             setCurrentView={setCurrentView}
             openModal={() => setModalState({ isOpen: true, changeId: null })}
+            closeSidebar={() => setIsSidebarOpen(false)}
           />
         </nav>
-        <main className={styles.mainContainer}>{currentViewComponent()}</main>
+        <main
+          className={`${styles.mainContainer} ? ${isSidebarOpen ? styles.mainDarkened : ""}`}
+        >
+          {currentViewComponent()}
+        </main>
         {modalState.isOpen && (
           <TaskModal
             isOpen={modalState.isOpen}
